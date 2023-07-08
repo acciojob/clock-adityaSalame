fa4bd8f44fd11f46ce0c149737c3a3706a94d75d
Aditya <121721549+adityaSalame@users.noreply.github.com>
2023-07-08 22:40:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let timer=document.createElement("P");
+timer.setAttribute("id","timer");
+document.body.appendChild(timer);
+function updateTimer() {
+            let now = new Date();
+            let date = now.toLocaleDateString();
+            let time = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+            let displayString = date + ', ' + time;
+
+            document.getElementById('timer').textContent = displayString;
+        }
+
+        // Run the updateTimer function every second
+        setInterval(updateTimer, 1000);
